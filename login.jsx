b2fb7dff4b693d8e5f766907efe87e6e215b294e
jwj <123456@qq.com>
2019-01-14 20:32:42 +0800

    ddd

--- login.jsx
+++ login.jsx
@@ -1,111 +1,78 @@
 import React, {Component} from 'react'
-import {
-  Form,
-  Icon,
-  Input,
-  Button
-} from 'antd'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {Form,Input,Icon,Button} from "antd";
 
-import logo from '../../assets/images/logo.png'
 import './index.less'
-
-const Item = Form.Item
-
-/*
-登陆的路由组件
- */
-export default class Login extends Component {
-  render() {
-    return (
-      <div className='login'>
-        <div className='login-header'>
-          <img src={logo} alt="logo"/>
-          React项目: 后台管理系统
+import img from '../../assets/images/logo.png'
+export default class Login extends Component{
+    render(){
+        return(
+        <div className='login '>
+            <header className='login-header'>
+                <img src={img} className='img'/>
+                React项目: 后台管理系
+            </header>
+            <div className='login-content'>
+             <div className='login-box'>
+                <div className='title'>用户登录</div>
+                <LoginForm/>
+             </div>
+            </div>
         </div>
-
-        <div className='login-content'>
-          <div className='login-box'>
-            <div className="title">用户登陆</div>
-            <LoginForm/>
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
-
-/*
-包含<Form>被包装组件
- */
-class LoginForm extends React.Component {
-
-  clickLogin = () => {
-
-    // 只有当验证没有错误时才输出输入的数据
-    this.props.form.validateFields((error, values) => {
-      console.log('validateFields', error, values)
-      if(!error) {
-        console.log('收集表单数据', values)
-      } else {
-        this.props.form.resetFields() // 重置所有输入框
-      }
-    })
-  }
-
-  checkPassword = (rule,value,callback) => { // 如果不满足要求, 通过调用callback()来指定对应的message
-    console.log('checkPassword()', rule, value)
-    if(!value) {
-      callback('必须输入密码')
-    } else if(value.length<4 || value.length>8) {
-      callback('密码必须是4到8位')
-    } else {
-      callback() // 如果不传参数代表成功
+        )
     }
-  }
-
-  render () {
-
-    const {getFieldDecorator } = this.props.form
-
-    // this.props.form.getFieldValue('username')
-
-    return (
-      <Form className='login-form'>
-        <Item>
-          {
-            getFieldDecorator('username', {
-              initialValue: 'admin', // input的默认值
-              rules: [ // 声明式验证配置
-                { type: "string", required: true, message: '必须输入用户名' },
-                { min: 4, message: '长度不能少于4位' },
-                ],
-            })(
-              <Input placeholder='请输入用户名' prefix={<Icon type="user"/> }/>
-            )
-          }
-
-        </Item>
-        <Form.Item>
-          {
-            getFieldDecorator('password', {
-              rules: [{validator: this.checkPassword}]  // 编程式验证
-            })(
-              <Input type='password' placeholder='请输入密码' prefix={<Icon type="lock" />} />
-            )
-          }
-
-
-        </Form.Item>
-        <Form.Item>
-          <Button type='primary' className='login-form-button' onClick={this.clickLogin}>登录</Button>
-        </Form.Item>
-      </Form>
-    )
-  }
 }
+//包含<Form>的被包装组件
+class LoginForm extends Component{
+    clicklogin = ()=> {
+        this.props.form.validateFields((err,values)=>{
+            if(!err){
+                console.log('11111111111',values)
+            }
+        })
 
-/*
-包装包含<Form>的组件, 生成一个新的组件(包装组件)
-包装组件会向被包装传递一个属性: form
- */
+    }
+    checkPassword = (rule,value,callback) =>{
+        if(!value){
+            callback('请输入密码')
+        }else if(value.length<4||value.length>8){
+            callback('密码长度位4-8位')
+        }else{
+            callback()
+        }
+    }
+
+    render(){
+        const {getFieldDecorator} = this.props.form
+        return(
+            <Form className='login-form'>
+                <Form.Item>
+                    {
+                        getFieldDecorator('username',{
+                            rules:[
+                                {required:true,message:'请输入用户名'},
+                                {min:4,message:'长度不能小于4位'}
+                                ]
+                        })(
+                            <Input placeholder='请输入用户名'prefix={<Icon type='username'/>}/>
+                        )
+                    }
+                </Form.Item>
+                <Form.Item>
+                    {
+                        getFieldDecorator('password',{
+                            rules: [{validator: this.checkPassword}]
+                        })(
+                            <Input placeholder='请输入密码'prefix={<Icon type='lock'/>}type='password'/>
+                        )
+                    }
+
+                </Form.Item>
+                <Button type='primary' className='login-form-button' onClick={this.clicklogin}>登录</Button>
+            </Form>
+        )
+    }
+}
+//包装包含<Form>的组件生成一个新的包装组件
+//会向被包装组件传递Form属性
 LoginForm = Form.create()(LoginForm)
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -14,20 +14,65 @@ export default class Login extends Component{
             </header>
             <div className='login-content'>
              <div className='login-box'>
-                        <div className='title'>用户登录</div>
-                        <Form className='login-form'>
-                            <Form.Item>
-                                <Input/>
-                            </Form.Item>
-                            <Form.Item>
-                                <Input/>
-                            </Form.Item>
-                            <Button className='login-form-button'>登录</Button>
-
-                        </Form>
-                    </div>
+                <div className='title'>用户登录</div>
+                <LoginForm/>
+             </div>
             </div>
         </div>
         )
     }
 }
+//包含<Form>的被包装组件
+class LoginForm extends Component{
+    clicklogin = ()=> {
+        this.props.form.validateFields((err,values)=>{
+            if(!err){
+                console.log('11111111111',values)
+            }
+        })
+
+    }
+    checkPassword = (rule,value,callback) =>{
+        if(!value){
+            callback('请输入密码')
+        }else if(value.length<4||value.length>8){
+            callback('密码长度位4-8位')
+        }else{
+            callback()
+        }
+    }
+
+    render(){
+        const {getFieldDecorator} = this.props.form
+        return(
+            <Form className='login-form'>
+                <Form.Item>
+                    {
+                        getFieldDecorator('username',{
+                            rules:[
+                                {required:true,message:'请输入用户名'},
+                                {min:4,message:'长度不能小于4位'}
+                                ]
+                        })(
+                            <Input placeholder='请输入用户名'prefix={<Icon type='username'/>}/>
+                        )
+                    }
+                </Form.Item>
+                <Form.Item>
+                    {
+                        getFieldDecorator('password',{
+                            rules: [{validator: this.checkPassword}]
+                        })(
+                            <Input placeholder='请输入密码'prefix={<Icon type='lock'/>}type='password'/>
+                        )
+                    }
+
+                </Form.Item>
+                <Button type='primary' className='login-form-button' onClick={this.clicklogin}>登录</Button>
+            </Form>
+        )
+    }
+}
+//包装包含<Form>的组件生成一个新的包装组件
+//会向被包装组件传递Form属性
+LoginForm = Form.create()(LoginForm)
